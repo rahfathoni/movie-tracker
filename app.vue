6232ea146aa4ffd514930e1257ae9d8f065f30bf
rahfathoni <rahfathoni@gmail.com>
2024-04-26 16:54:02 +0700

    develop sidebar: navigation, UI

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
   <NuxtLayout >
-    <Sidebar />
-    <Header />
-    <NuxtPage />
+    <div class="grid grid-cols-12">
+      <Sidebar class="col-span-1 max-w-[60px]"/>
+      <div class="col-span-11">
+        <Header />
+        <NuxtPage />
+      </div>
+    </div>
   </NuxtLayout>
 </template>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="text-blue-500">
-    navbar
+  <nav class="bg-cust-black text-white flex flex-col justify-around items-center h-screen text-[21px]">
+    <NuxtLink to="/"><IconsHome /></NuxtLink>
+    <NuxtLink to="/movie"><IconsMovie /></NuxtLink>
+    <NuxtLink to="/tvshow"><IconsDeviceTV /></NuxtLink>
+    <NuxtLink to="/search"><IconsSearch /></NuxtLink>
   </nav>
 </template>
