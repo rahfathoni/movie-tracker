7936af64bde68071de9ffe300816c4fab931fa9e
rahfathoni <rahfathoni@gmail.com>
2024-04-26 18:02:26 +0700

    develop header: ui

--- app.vue
+++ app.vue
@@ -2,8 +2,8 @@
   <NuxtLayout >
     <div class="grid grid-cols-12">
       <Sidebar class="col-span-1 max-w-[60px]"/>
-      <div class="col-span-11">
-        <Header />
+      <div class="col-span-11 ml-none lg:ml-[-30px]">
+        <MainHeader />
         <NuxtPage />
       </div>
     </div>
